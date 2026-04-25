72b117079810aa0384edb46aa95587e00f5f9530
Private <private@invalid.local>
2026-04-25 14:33:13 +0000

    fixed one warning

--- src/core/fs.rs
+++ src/core/fs.rs
@@ -4,7 +4,7 @@ use std::{
     collections::{BTreeSet, HashSet},
     env,
     fs::{self, File, Metadata},
-    io::{BufRead, BufReader, Lines},
+    io::{BufRead, BufReader},
     path::PathBuf,
 };
 
