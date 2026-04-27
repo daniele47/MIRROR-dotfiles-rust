d20fb2271e5be742ef930b19f05bdaa930f7a72b
Private <private@invalid.local>
2026-04-27 10:52:16 +0000

    updated main file for more useful testing binary

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
-use autosaver::core::{errors::Result, fs::AbsPath};
+use autosaver::core::{
+    errors::Result,
+    fs::{AbsPath, LineWriter},
+};
+
+fn purge_path_even_on_panic(tmpdir: &AbsPath) -> impl Drop {
+    struct Guard(AbsPath);
+    impl Drop for Guard {
+        fn drop(&mut self) {
+            let _ = self.0.purge_path(true);
+        }
+    }
+    Guard(tmpdir.clone())
+}
 
 fn main() -> Result<()> {
-    let abs = AbsPath::from("/etc/passwd");
-    abs.create_file(true)?;
-    for line in abs.read_lines()? {
-        let line = line?;
-        println!("{line}");
+    println!("Binary version: {}", env!("CARGO_PKG_VERSION"));
+
+    let tmpdir = AbsPath::new_tmp("rust_example");
+    tmpdir.create_dir()?;
+    let _guard = purge_path_even_on_panic(&tmpdir);
+
+    let tmpfile = tmpdir.joins(&["tmpfile.txt"]);
+    let mut writer = tmpfile.write_lines()?;
+    writer.write_line("Line 1")?;
+    writer.write_line("Line 2")?;
+    writer.write_line("It's the final line!!!")?;
+    writer.flush()?;
+
+    println!("\nReading {}:", tmpfile.to_str_lossy());
+    for line in tmpfile.read_lines()? {
+        println!("{}", line?);
     }
+
     Ok(())
 }
