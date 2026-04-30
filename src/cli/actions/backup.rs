39af9a31014c220b0fb72b561a57cef3fb9d5f3b
Private <private@invalid.local>
2026-04-30 16:13:24 +0000

    added parsing in backup action

--- src/cli/actions/backup.rs
+++ src/cli/actions/backup.rs
@@ -1,6 +1,7 @@
 use crate::cli::{
     actions::Runner,
     error::{Error, Result},
+    flags::Flag,
     output::Renderer,
 };
 
@@ -9,6 +10,26 @@ where
     I: Renderer<Error = Error>,
 {
     pub fn backup(&mut self) -> Result<()> {
+        let mut iter = self.args.params().iter();
+        let arg_command = iter.next().map(String::as_str).unwrap_or_default();
+        let arg_profile = iter.next().map(String::as_str).unwrap_or_default();
+        let wflag_y = self.args.flags().contains(&Flag::Word("assumeyes".into()));
+        let lflag_y = self.args.flags().contains(&Flag::Letter('y'));
+        let flag_y = wflag_y || lflag_y;
+        let wflag_n = self.args.flags().contains(&Flag::Word("assumeno".into()));
+        let lflag_n = self.args.flags().contains(&Flag::Letter('n'));
+        let flag_n = wflag_n || lflag_n;
+        let wflag_diff = self.args.flags().contains(&Flag::Word("diff".into()));
+        let lflag_diff = self.args.flags().contains(&Flag::Letter('d'));
+        let flag_diff = wflag_diff || lflag_diff;
+        let wflag_all = self.args.flags().contains(&Flag::Word("all".into()));
+        let lflag_all = self.args.flags().contains(&Flag::Letter('a'));
+        let flag_all = wflag_all || lflag_all;
+        let flag_notdiff = self.args.flags().contains(&Flag::Word("notdiff".into()));
+        let flag_track = self.args.flags().contains(&Flag::Word("track".into()));
+
+        #[rustfmt::skip]
+        println!("{arg_command} {arg_profile} {flag_y} {flag_n} {flag_diff} {flag_all} {flag_notdiff} {flag_track} {:?}", Self::paths("home"));
         todo!()
     }
 }
