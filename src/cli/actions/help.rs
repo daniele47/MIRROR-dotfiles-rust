5940033b5d6aeb0abf9dad42b1bac48fe982724f
Private <private@invalid.local>
2026-05-02 05:50:22 +0000

    compacted help code

--- src/cli/actions/help.rs
+++ src/cli/actions/help.rs
@@ -8,32 +8,26 @@ impl<I: InOut> Runner<I> {
         let _ = self.args.params().get(1).map(String::as_ref).unwrap_or("");
         let _ = self.args.params().get(2).map(String::as_ref).unwrap_or("");
         let col = Self::HELP_COLOR;
+        let io = &mut self.inout;
         match first {
             _ => {
-                self.inout.writeln("Commands:", col);
-                self.inout.write("  list        ", col);
-                self.inout.writeln(
-                    "Show differences between home and backup files for a profile",
-                    &[],
-                );
-                self.inout.write("  save        ", col);
-                self.inout
-                    .writeln("Save changes from the home directory to the backup", &[]);
-                self.inout.write("  restore     ", col);
-                self.inout
-                    .writeln("Restore changes from the backup to the home directory", &[]);
-                self.inout.write("  delete      ", col);
-                self.inout
-                    .writeln("Delete files from both home and backup directories", &[]);
-                self.inout.writeln("", &[]);
-                self.inout.writeln("Flags:", col);
-                self.inout.write("  --help -h   ", col);
-                self.inout
-                    .writeln("Print the help message for commands and subcommands", &[]);
-                self.inout.write("  --version   ", col);
-                self.inout.writeln("Print the binary version", &[]);
-                self.inout.write("  --nocolor   ", col);
-                self.inout.writeln("Avoid all colors in the output", &[]);
+                io.writeln("Commands:", col);
+                io.write("  list        ", col);
+                io.writeln("Show differences between home and backup files", &[]);
+                io.write("  save        ", col);
+                io.writeln("Save changes from the home directory to the backup", &[]);
+                io.write("  restore     ", col);
+                io.writeln("Restore changes from the backup to the home directory", &[]);
+                io.write("  delete      ", col);
+                io.writeln("Delete files from both home and backup directories", &[]);
+                io.writeln("", &[]);
+                io.writeln("Flags:", col);
+                io.write("  --help -h   ", col);
+                io.writeln("Print the help message for commands and subcommands", &[]);
+                io.write("  --version   ", col);
+                io.writeln("Print the binary version", &[]);
+                io.write("  --nocolor   ", col);
+                io.writeln("Avoid all colors in the output", &[]);
             }
         }
         Ok(())
